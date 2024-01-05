fec69572d8e644e75d38a10f9bd1b74691dc8866
tural.shkrov <tural.shkrov@gmail.com>
2024-01-05 23:49:07 +0400

    binary search tree

--- binary-search-trees/app.js
+++ binary-search-trees/app.js
@@ -5,3 +5,45 @@ class Node {
     this.right = right;
   }
 }
+
+class Tree {
+  constructor(arr) {
+    this.arr = arr;
+    this.sortedArr = this.#arrSort()
+    this.root = this.buildTree(this.sortedArr);
+  }
+
+  buildTree(arr) {
+    const mid = Math.floor(arr.length / 2);
+    return arr[mid];
+  }
+
+  #arrSort() {
+    for (let i = 0; i < this.arr.length; i++) {
+      const elementI = this.arr[i];
+      for (let j = i + 1; j < this.arr.length; j++) {
+        const elementJ = this.arr[j];
+        if (elementI === elementJ) {
+          this.arr = this.arr.filter((_, index) => index !== j)
+        }
+      }
+    }
+    return this.arr.sort((a, b) => a - b)
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+}
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log(tree.root);
