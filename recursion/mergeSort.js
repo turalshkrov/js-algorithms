958c10e49a30739aa474ef8db198cb633312c28a
tural.shkrov <tural.shkrov@gmail.com>
2024-01-15 23:59:22 +0400

    mergesort

--- recursion/mergeSort.js
+++ recursion/mergeSort.js
@@ -1,40 +1,39 @@
 const array = [1, 3, 9, 6, 5 ,7 ,8 ,2, 4];
 
-const merge = (arrL, arrR) => {
+const mergeRec = (array) => {
+  if (array.length === 1) {
+    return array;
+  }
+  if (array.length > 1) {
+    const mid = Math.floor(array.length / 2);
+    const leftArray = array.slice(0, mid);
+    const rightArray = array.slice(mid, array.length);
+    return merge(mergeRec(leftArray), mergeRec(rightArray));
+  }
+}
+
+const merge = (leftArray, rightArray) => {
   const result = [];
   let iR = 0;
   let iL = 0;
-  while (iL < arrL.length && iR < arrR.length) {
-    if(arrL[iL] < arrR[iR]){
-      result.push(arrL[iL]);
-      iL += 1;
+  while (iL < leftArray.length && iR < rightArray.length) {
+    if (leftArray[iL] < rightArray[iR]) {
+      result.push(leftArray[iL]);
+      iL++;
     } else {
-      result.push(arrR[iR]);
-      iR += 1;
+      result.push(rightArray[iR]);
+      iR++
     }
   }
-  while (iL < arrL.length) {
-		result.push(arrL[iL]);
-		iL++;
-	}
-
-	while (iR < arrR.length) {
-		result.push(arrR[iR]);
-		iR++;
-	}
+  while (iL < leftArray.length) {
+    result.push(leftArray[iL]);
+    iL++;
+  }
+  while (iR < rightArray.length) {
+    result.push(rightArray[iR]);
+    iR++;
+  }
   return result;
 }
 
-const mergesortRec = (array) => {
-  if (array.length === 1) {
-    return array
-  }
-  if (array.length > 1) {
-    const mid = Math.floor(array.length / 2)
-    const arrayL = array.slice(0, mid);
-    const arrayR = array.slice(mid, array.length);
-    return merge(mergesortRec(arrayL), mergesortRec(arrayR));
-  } 
-}
-
-console.log(mergesortRec(array))
+console.log(mergeRec(array));
